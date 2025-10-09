4401e08ea4ac7436706688f9572e8e35d2974f91
Shehbaz  Khan <shehbaz.khan@Yash.com>
2025-10-09 19:13:16 +0530

    updated project to latest nx version

--- angular-schematics/src/application/index.ts
+++ angular-schematics/src/application/index.ts
@@ -1,197 +1,162 @@
 import { strings } from "@angular-devkit/core";
 import {
-  apply,
-  applyTemplates,
-  chain,
-  externalSchematic,
-  MergeStrategy,
-  mergeWith,
-  move,
-  noop,
-  Rule,
-  SchematicContext,
-  Tree,
-  url,
+ apply,
+ applyTemplates,
+ chain,
+ externalSchematic,
+ MergeStrategy,
+ mergeWith,
+ move,
+ noop,
+ Rule,
+ SchematicContext,
+ Tree,
+ url,
 } from "@angular-devkit/schematics";
-import {
-  addDepsToPackageJson,
-  formatFiles,
-  getWorkspace,
-} from "@nrwl/workspace";
+
+import { getWorkspace } from "@schematics/angular/utility/workspace";
 import { join, normalize } from "path";
 import { addImportToAppModule, insertStatement } from "../utils/utils";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function application(options: any): Rule {
-  return async (host: Tree, _context: SchematicContext) => {
-    const workspace = await getWorkspace(host);
-    const newProjectRoot =
-      (workspace.extensions.newProjectRoot as string | undefined) ?? "";
-    const isRootApp = options.projectRoot !== undefined;
-    const appDir = isRootApp
-      ? normalize(options.projectRoot || "")
-      : join(normalize(newProjectRoot), strings.dasherize(options.name));
-    options.appDir = appDir;
+ return async (host: Tree, _context: SchematicContext) => {
+  const workspace = await getWorkspace(host);
+  const newProjectRoot =
+   (workspace.extensions.newProjectRoot as string | undefined) ?? "";
+  const isRootApp = options.projectRoot !== undefined;
+  const appDir = isRootApp
+   ? normalize(options.projectRoot || "")
+   : join(normalize(newProjectRoot), strings.dasherize(options.name));
+  options.appDir = appDir;
 
-    let originalOptionsObject = JSON.parse(JSON.stringify(options));
-    const keysToDelete = ["framework", "ngrx", "i18n", "appDir"];
-    /*--get schema compatible with application schema---*/
-    let inputToApplicationSchema = deleteKeys(options, keysToDelete);
+  let originalOptionsObject = JSON.parse(JSON.stringify(options));
+  const keysToDelete = ["framework", "ngrx", "i18n", "appDir"];
+  /*--get schema compatible with application schema---*/
+  let inputToApplicationSchema = deleteKeys(options, keysToDelete);
 
-    return chain([
-      externalSchematic("@schematics/angular", "application", {
-        ...inputToApplicationSchema,
-        style: "scss",
-        skipInstall: true,
-        strict: true,
-      }),
-      addImportToAppModule("SharedModule", "./shared/shared.module"),
-      addImportToAppModule("CoreModule", "./core/core.module"),
-      originalOptionsObject.i18n ? addI18n() : noop,
-      mergeWith(
-        apply(url("./files"), [
-          applyTemplates({
-            utils: strings,
-            ...originalOptionsObject,
-            appName: originalOptionsObject.name,
-            isRootApp,
-          }),
-          move(appDir),
-        ]),
-        MergeStrategy.Overwrite
-      ),
-      originalOptionsObject.i18n
-        ? mergeWith(
-            apply(url("./i18n-files"), [
-              applyTemplates({
-                utils: strings,
-                ...originalOptionsObject,
-                appName: originalOptionsObject.name,
-                isRootApp,
-              }),
-              move(appDir),
-            ]),
-            MergeStrategy.Overwrite
-          )
-        : noop,
-      addDevdependenciesToPackageJSON(),
-      setFramework(
-        originalOptionsObject,
-        isRootApp,
-        appDir,
-        originalOptionsObject.i18n
-      ),
-      setNGRX(originalOptionsObject),
+  return chain([
+   externalSchematic("@schematics/angular", "application", {
+    ...inputToApplicationSchema,
+    style: "scss",
+    skipInstall: true,
+    strict: true,
+   }),
+   addImportToAppModule("SharedModule", "./shared/shared.module"),
+   addImportToAppModule("CoreModule", "./core/core.module"),
+   originalOptionsObject.i18n ? addI18n() : noop,
+   mergeWith(
+    apply(url("./files"), [
+     applyTemplates({
+      utils: strings,
+      ...originalOptionsObject,
+      appName: originalOptionsObject.name,
+      isRootApp,
+     }),
+     move(appDir),
+    ]),
+    MergeStrategy.Overwrite
+   ),
+   originalOptionsObject.i18n
+    ? mergeWith(
+       apply(url("./i18n-files"), [
+        applyTemplates({
+         utils: strings,
+         ...originalOptionsObject,
+         appName: originalOptionsObject.name,
+         isRootApp,
+        }),
+        move(appDir),
+       ]),
+       MergeStrategy.Overwrite
+      )
+    : noop,
+   addDevdependenciesToPackageJSON(),
+   setFramework(
+    originalOptionsObject,
+    isRootApp,
+    appDir,
+    originalOptionsObject.i18n
+   ),
+   setNGRX(originalOptionsObject),
 
-      formatFiles(),
-    ]);
-  };
+   (tree: Tree) => tree,
+  ]);
+ };
 }
 
 export function addDevdependenciesToPackageJSON() {
-  return chain([
-    addDepsToPackageJson(
-      {},
-      {
-        "@types/node": "^12.11.1",
-      }
-    ),
-  ]);
+ return noop;
 }
 
 export function setFramework(
-  _options: any,
-  isRootApp: boolean,
-  appDir: string,
-  isI18nSeleted: boolean
+ _options: any,
+ isRootApp: boolean,
+ appDir: string,
+ isI18nSeleted: boolean
 ): Rule {
-  if (_options.framework === "material") {
-    return chain([addMaterialToPackageJson(), updateStyles(_options)]);
-  }
+ if (_options.framework === "material") {
+  return chain([addMaterialToPackageJson(), updateStyles(_options)]);
+ }
 
-  if (_options.framework === "bootstrap") {
-    return chain([addBootstrapToPackageJson(), updateStyles(_options)]);
-  }
+ if (_options.framework === "bootstrap") {
+  return chain([addBootstrapToPackageJson(), updateStyles(_options)]);
+ }
 
-  if (_options.framework === "tailwind") {
-    return chain([
-      addTailwindToPackageJson(),
-      updateStyles(_options),
-      createTailwindConfig(_options, isRootApp, appDir, isI18nSeleted),
-    ]);
-  }
+ if (_options.framework === "tailwind") {
+  return chain([
+   addTailwindToPackageJson(),
+   updateStyles(_options),
+   createTailwindConfig(_options, isRootApp, appDir, isI18nSeleted),
+  ]);
+ }
 
-  return noop;
+ return noop;
 }
 
 export function setNGRX(_options: any): Rule {
-  if (!_options.ngrx) {
-    return noop;
-  }
-  return chain([
-    externalSchematic("@nrwl/angular", "ngrx", {
-      name: "app",
-      module: "src/app/app.module.ts",
-      facade: true,
-      root: true,
-    }),
-    externalSchematic("@nrwl/angular", "ngrx", {
-      name: "app",
-      module: "src/app/core/core.module.ts",
-      facade: true,
-      root: false,
-    }),
-    addDepsToPackageJson(
-      {
-        "@nrwl/angular": "~14.7.0",
-        "@nrwl/workspace": "~14.7.0",
-      },
-      {}
-    ),
-  ]);
+ if (!_options.ngrx) {
+  return noop;
+ }
+ return chain([
+  externalSchematic("@nx/angular", "ngrx", {
+   name: "app",
+   module: "src/app/app.module.ts",
+   facade: true,
+   root: true,
+  }),
+  externalSchematic("@nx/angular", "ngrx", {
+   name: "app",
+   module: "src/app/core/core.module.ts",
+   facade: true,
+   root: false,
+  }),
+  noop,
+ ]);
 }
 
 export function addMaterialToPackageJson(): Rule {
-  return addDepsToPackageJson(
-    {
-      "@angular/material": "~14.2.0",
-      "@angular/cdk": "~14.2.0",
-    },
-    {},
-    false
-  );
+ return noop;
 }
 
 export function addBootstrapToPackageJson(): Rule {
-  return addDepsToPackageJson(
-    {
-      bootstrap: "^4.0.0",
-    },
-    {},
-    false
-  );
+ return noop;
 }
 export function addTailwindToPackageJson(): Rule {
-  return addDepsToPackageJson(
-    {
-      tailwindcss: "^2.2.0",
-    },
-    {},
-    false
-  );
+ return noop;
 }
 
 export function updateStyles(options: any) {
-  return (host: Tree) => {
-    let content = `
+ return (host: Tree) => {
+  let content = `
     @import './app/app.theme';
 
     @include app-theme();
 
   `;
-    if (options.framework === "material") {
-      content = `
+  if (options.framework === "material") {
+   content = `
     @import "@angular/material/prebuilt-themes/indigo-pink.css";
     body {
       height: 100%;
@@ -202,58 +167,58 @@ export function updateStyles(options: any) {
     }
     ${content}
     `;
-    }
-    if (options.framework === "bootstrap") {
-      content = `
+  }
+  if (options.framework === "bootstrap") {
+   content = `
     @import "~bootstrap/dist/css/bootstrap.css";
     ${content}
     `;
-    }
-    if (options.framework === "tailwind") {
-      content = `
+  }
+  if (options.framework === "tailwind") {
+   content = `
       @import 'tailwindcss/base';
       @import 'tailwindcss/components';
       @import 'tailwindcss/utilities';
     ${content}
     `;
-    }
-    host.overwrite(`src/styles.scss`, content);
-    return host;
-  };
+  }
+  host.overwrite(`src/styles.scss`, content);
+  return host;
+ };
 }
 
 export function createTailwindConfig(
-  _options: any,
-  isRootApp: boolean,
-  appDir: string,
-  isI18nSeleted: boolean
+ _options: any,
+ isRootApp: boolean,
+ appDir: string,
+ isI18nSeleted: boolean
 ): Rule {
-  let inputUrl = isI18nSeleted
-    ? "./tailwind-files/tailwind+i18n-files/"
-    : "./tailwind-files/tailwind-i18n-files/";
-  return mergeWith(
-    apply(url(inputUrl), [
-      applyTemplates({
-        utils: strings,
-        ..._options,
-        appName: _options.name,
-        isRootApp,
-      }),
-      move(appDir),
-    ]),
-    MergeStrategy.Overwrite
-  );
+ let inputUrl = isI18nSeleted
+  ? "./tailwind-files/tailwind+i18n-files/"
+  : "./tailwind-files/tailwind-i18n-files/";
+ return mergeWith(
+  apply(url(inputUrl), [
+   applyTemplates({
+    utils: strings,
+    ..._options,
+    appName: _options.name,
+    isRootApp,
+   }),
+   move(appDir),
+  ]),
+  MergeStrategy.Overwrite
+ );
 }
 
 export function addI18n(): Rule {
-  return chain([
-    addImportToAppModule(
-      `
+ return chain([
+  addImportToAppModule(
+   `
          TranslateModule,
          TranslateLoader
          `,
-      "@ngx-translate/core",
-      `
+   "@ngx-translate/core",
+   `
         TranslateModule.forRoot({
           loader: {
             provide: TranslateLoader,
@@ -262,32 +227,26 @@ export function addI18n(): Rule {
           }
         })
         `
-    ),
-    addImportToAppModule(`HttpClientModule`, "@angular/common/http"),
-    addImportToAppModule(`HttpClient`, "@angular/common/http", undefined, true),
-    addImportToAppModule(
-      `TranslateHttpLoader`,
-      "@ngx-translate/http-loader",
-      undefined,
-      true
-    ),
-    insertStatement(
-      "src/app/app.module.ts",
-      `export function translateHttpLoaderFactory(http: HttpClient) {
+  ),
+  addImportToAppModule(`HttpClientModule`, "@angular/common/http"),
+  addImportToAppModule(`HttpClient`, "@angular/common/http", undefined, true),
+  addImportToAppModule(
+   `TranslateHttpLoader`,
+   "@ngx-translate/http-loader",
+   undefined,
+   true
+  ),
+  insertStatement(
+   "src/app/app.module.ts",
+   `export function translateHttpLoaderFactory(http: HttpClient) {
           return new TranslateHttpLoader(http);
         }`
-    ),
-    addDepsToPackageJson(
-      {
-        "@ngx-translate/core": "13.0.0",
-        "@ngx-translate/http-loader": "6.0.0",
-      },
-      {}
-    ),
-  ]);
+  ),
+  noop,
+ ]);
 }
 
 function deleteKeys(inputObj: any, keysToDelete: string[]): any {
-  keysToDelete.forEach((key) => delete inputObj[key]);
-  return inputObj;
+ keysToDelete.forEach((key) => delete inputObj[key]);
+ return inputObj;
 }
--- cli/generators/react.ts
+++ cli/generators/react.ts
@@ -2,176 +2,176 @@ import { execSync } from "child_process";
 import inquirer = require("inquirer");
 
 export function reactAppGenerator() {
-  getArgs().then((a) => {
-    execSync(
-      `npx create-nx-workspace@13.0.0 ${a.workspace}  --preset empty --nx-cloud=false`,
-      {
-        cwd: process.cwd(),
-        stdio: [0, 1, 2],
-      }
-    );
-    execSync(
-      `npm install @angular-devkit/core@20.0.2 @angular-devkit/schematics@20.0.2 --force`,
-      {
-        cwd: `${process.cwd()}/${a.workspace}`,
-        stdio: [0, 1, 2],
-      }
-    );
+ getArgs().then((a: any) => {
+  execSync(
+   `npx create-nx-workspace@latest ${a.workspace}  --preset empty --nx-cloud=false`,
+   {
+    cwd: process.cwd(),
+    stdio: [0, 1, 2],
+   }
+  );
+  execSync(
+   `npm install @angular-devkit/core@latest @angular-devkit/schematics@latest --force`,
+   {
+    cwd: `${process.cwd()}/${a.workspace}`,
+    stdio: [0, 1, 2],
+   }
+  );
 
-    execSync(`npm install @ptg-ui/react-schematics --force`, {
-      cwd: `${process.cwd()}/${a.workspace}`,
-      stdio: [0, 1, 2],
-    });
-
-    execSync(
-      `nx generate @ptg-ui/react-schematics:application --name ${a.name} --style ${a.style} --framework ${a.framework} --routing ${a.routing} --redux ${a.redux} --i18n ${a.i18n} --auth ${a.auth}`,
-      {
-        cwd: `${process.cwd()}/${a.workspace}`,
-        stdio: [0, 1, 2],
-      }
-    );
-
-    const npmPkgs = [];
-    if (a.auth) {
-      if (a.auth === "msal") {
-        npmPkgs.push("@azure/msal-react");
-        npmPkgs.push("@azure/msal-browser");
-      } else if (a.auth === "okta") {
-        npmPkgs.push("@okta/okta-auth-js");
-        npmPkgs.push("@okta/okta-react");
-      }
-    }
-
-    if (a.routing) {
-      npmPkgs.push("react-router-dom@6.28.0");
-    }
-    execSync(`npm install --f ${npmPkgs.join(" ")}`, {
-      cwd: `${process.cwd()}/${a.workspace}`,
-      stdio: [0, 1, 2],
-    });
+  execSync(`npm install @ptg-ui/react-schematics --force`, {
+   cwd: `${process.cwd()}/${a.workspace}`,
+   stdio: [0, 1, 2],
   });
+
+  execSync(
+   `nx generate @ptg-ui/react-schematics:application --name ${a.name} --style ${a.style} --framework ${a.framework} --routing ${a.routing} --redux ${a.redux} --i18n ${a.i18n} --auth ${a.auth}`,
+   {
+    cwd: `${process.cwd()}/${a.workspace}`,
+    stdio: [0, 1, 2],
+   }
+  );
+
+  const npmPkgs = [];
+  if (a.auth) {
+   if (a.auth === "msal") {
+    npmPkgs.push("@azure/msal-react@latest");
+    npmPkgs.push("@azure/msal-browser@latest");
+   } else if (a.auth === "okta") {
+    npmPkgs.push("@okta/okta-auth-js@latest");
+    npmPkgs.push("@okta/okta-react@latest");
+   }
+  }
+
+  if (a.routing) {
+   npmPkgs.push("react-router-dom@latest");
+  }
+  execSync(`npm install --f ${npmPkgs.join(" ")}`, {
+   cwd: `${process.cwd()}/${a.workspace}`,
+   stdio: [0, 1, 2],
+  });
+ });
 }
 
 function getArgs() {
-  const frameWorkOptions: { value: string; label: string }[] = [
-    {
-      value: "none",
-      label: "None",
-    },
-    {
-      value: "material",
-      label: "Material",
-    },
-    {
-      value: "bootstrap",
-      label: "Bootstrap",
-    },
-  ];
-  const styleOptions: { value: string; label: string }[] = [
-    {
-      value: "css",
-      label: "CSS",
-    },
-    {
-      value: "scss",
-      label: "SASS(.scss)",
-    },
-    {
-      value: "styl",
-      label: "Stylus(.styl)",
-    },
-    {
-      value: "less",
-      label: "LESS",
-    },
-  ];
-  const authOptions: { value: string; label: string }[] = [
-    {
-      value: "custom",
-      label: "Custom",
-    },
-    {
-      value: "msal",
-      label: "Msal",
-    },
-    {
-      value: "okta",
-      label: "Okta",
-    },
-  ];
-  return inquirer
-    .prompt([
-      {
-        name: "workspace",
-        message: `Workspace name (e.g., org name)`,
-        type: "string",
-      },
-      {
-        name: "name",
-        message: `What name would you like to use for the application?`,
-        type: "string",
-      },
-      {
-        name: "framework",
-        message: `Which framework would you like to use?`,
-        type: "list",
-        default: "none",
-        choices: frameWorkOptions,
-      },
-      {
-        name: "auth",
-        message: `Which Authentication you would like to add to this Application??`,
-        type: "list",
-        default: "custom",
-        choices: authOptions,
-      },
-      {
-        name: "style",
-        message: `Which stylesheet format would you like to use?`,
-        type: "list",
-        default: "css",
-        choices: styleOptions,
-      },
-      {
-        name: "routing",
-        message: "Would you like to add React Router to this application?",
-        type: "confirm",
-      },
-      {
-        name: "redux",
-        message: "Would you like to add Redux to this application?",
-        type: "confirm",
-      },
-      {
-        name: "i18n",
-        message: "would you like to Adds i18n in project?",
-        type: "confirm",
-      },
-    ])
-    .then((a) => {
-      return a;
-    });
-}
-
-function addVSCodeExtensions() {
-  const extensionsList = [
-    "mrmlnc.vscode-scss",
-    "esbenp.prettier-vscode",
-    "ms-vscode.vscode-typescript-tslint-plugin",
-    "vscode-icons-team.vscode-icons",
-    "dsznajder.es7-react-js-snippets",
-    "burkeholland.simple-react-snippets",
-  ];
-  const extensions = extensionsList
-    .map((ext) => `--install-extension ${ext}`)
-    .join(" ");
-  execSync(`code ${extensions}`, {
-    // stdio: [0, 1, 2],
-    cwd: process.cwd(),
+ const frameWorkOptions: { value: string; label: string }[] = [
+  {
+   value: "none",
+   label: "None",
+  },
+  {
+   value: "material",
+   label: "Material",
+  },
+  {
+   value: "bootstrap",
+   label: "Bootstrap",
+  },
+ ];
+ const styleOptions: { value: string; label: string }[] = [
+  {
+   value: "css",
+   label: "CSS",
+  },
+  {
+   value: "scss",
+   label: "SASS(.scss)",
+  },
+  {
+   value: "styl",
+   label: "Stylus(.styl)",
+  },
+  {
+   value: "less",
+   label: "LESS",
+  },
+ ];
+ const authOptions: { value: string; label: string }[] = [
+  {
+   value: "custom",
+   label: "Custom",
+  },
+  {
+   value: "msal",
+   label: "Msal",
+  },
+  {
+   value: "okta",
+   label: "Okta",
+  },
+ ];
+ return (inquirer as any)
+  .prompt([
+   {
+    name: "workspace",
+    message: `Workspace name (e.g., org name)`,
+    type: "string",
+   },
+   {
+    name: "name",
+    message: `What name would you like to use for the application?`,
+    type: "string",
+   },
+   {
+    name: "framework",
+    message: `Which framework would you like to use?`,
+    type: "list",
+    default: "none",
+    choices: frameWorkOptions,
+   },
+   {
+    name: "auth",
+    message: `Which Authentication you would like to add to this Application??`,
+    type: "list",
+    default: "custom",
+    choices: authOptions,
+   },
+   {
+    name: "style",
+    message: `Which stylesheet format would you like to use?`,
+    type: "list",
+    default: "css",
+    choices: styleOptions,
+   },
+   {
+    name: "routing",
+    message: "Would you like to add React Router to this application?",
+    type: "confirm",
+   },
+   {
+    name: "redux",
+    message: "Would you like to add Redux to this application?",
+    type: "confirm",
+   },
+   {
+    name: "i18n",
+    message: "would you like to Adds i18n in project?",
+    type: "confirm",
+   },
+  ])
+  .then((a: any) => {
+   return a;
   });
 }
 
+function addVSCodeExtensions() {
+ const extensionsList = [
+  "mrmlnc.vscode-scss",
+  "esbenp.prettier-vscode",
+  "ms-vscode.vscode-typescript-tslint-plugin",
+  "vscode-icons-team.vscode-icons",
+  "dsznajder.es7-react-js-snippets",
+  "burkeholland.simple-react-snippets",
+ ];
+ const extensions = extensionsList
+  .map((ext) => `--install-extension ${ext}`)
+  .join(" ");
+ execSync(`code ${extensions}`, {
+  // stdio: [0, 1, 2],
+  cwd: process.cwd(),
+ });
+}
+
 export function invokeReactGenerator() {
-  reactAppGenerator();
-  addVSCodeExtensions();
+ reactAppGenerator();
+ // addVSCodeExtensions();
 }
